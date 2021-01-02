3bc19b2d70651e5f725742ee9863193ce3586e0e
Amira Rashad <amira.rashad1996@hotmail.com>
2021-01-02 19:18:00 +0500

    Update Generic.js

--- src/services/Generic.js
+++ src/services/Generic.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //axios.defaults.baseURL="http://localhost:4000/api/";
-axios.defaults.baseURL= "https://terminal-api1.herokuapp.com/";
+axios.defaults.baseURL= "https://terminal-api1.herokuapp.com/api/products/";
 
 
 
